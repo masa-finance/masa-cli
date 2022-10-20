ffa121661f9d83c44a62bab01193a3694645cc7f
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2022-10-20 15:48:24 +0200

    more version outputs

--- src/commands/version.ts
+++ src/commands/version.ts
@@ -1,9 +1,17 @@
 import { version as contractsVersion } from "@masa-finance/masa-contracts-identity/package.json";
 import { version as sdkVersion } from "@masa-finance/tools/package.json";
 import { version as cliVersion } from "../../package.json";
+import { config } from "../utils/storage";
 
 export const version = async () => {
   console.log(
     `CLI: ${cliVersion} Contracts: ${contractsVersion} SDK: ${sdkVersion}`
   );
+  console.log(
+    `Arweave: ${config.get("arweave-protocol")}://${config.get(
+      "arweave-host"
+    )}:${config.get("arweave-port")}`
+  );
+  console.log(`RPC Endpoint: ${config.get("rpc-url")}`);
+  console.log(`Masa Endpoint: ${config.get("api-url")}`);
 };
