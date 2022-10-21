06400fcfa5df110730538f620c36d5ff794296c8
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2022-10-21 18:08:09 +0200

    fix login

--- src/helpers/sdk/sdk.ts
+++ src/helpers/sdk/sdk.ts
@@ -1,12 +1,12 @@
 import { ethers } from "ethers";
-import { login } from "../../commands";
 import { logout } from "./session/logout";
 import { checkLogin } from "./session/check-login";
 import { creditScoreMint, sessionLogout } from "./helpers/client";
 import { loadIdentity } from "./identity/load-identity";
 import { loadIdentityContracts } from "./helpers/contracts";
 import { addresses } from "@masa-finance/tools";
-import { version } from "./helpers/version"
+import { version } from "./helpers/version";
+import { login } from "./session/login";
 
 export default class Masa {
   _provider;
