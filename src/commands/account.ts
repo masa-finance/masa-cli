d151b4a65755440e42d07d10296c8bd59922209d
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2022-10-27 12:17:10 +0200

    make account out prettier

--- src/commands/account.ts
+++ src/commands/account.ts
@@ -4,27 +4,43 @@ import { masa } from "../helpers/masa";
 
 export const account = async () => {
   const address = await masa.config.wallet.getAddress();
+  console.log(`Address: '${address}'`);
+
+  console.log(`\nNetwork: '${config.get("network")}'`);
 
   // login status
   const isLoggedIn = await masa.session.checkLogin();
-  console.log(`\nNetwork: ${config.get("network")}`);
-
   console.log(`Logged in: ${isLoggedIn}`);
 
   // identity id
   const identityId = await masa.identity.load(address);
-
-  console.log(`Identity ID: ${identityId}`);
-
-  // balances
-  console.log("\nBalances");
+  console.log(`Identity ID: '${identityId}'`);
 
   const balances = await masa.account.getBalances(address);
 
+  const precision = 8;
   if (balances) {
-    console.log(`ETH: ${ethers.utils.formatEther(balances.ethBalance)}`);
-    console.log(`MASA: ${ethers.utils.formatEther(balances.masaBalance)}`);
-    console.log(`USDC: ${ethers.utils.formatEther(balances.usdcBalance)}`);
-    console.log(`WETH: ${ethers.utils.formatEther(balances.wethBalance)}`);
+    // balances
+    console.log("\nBalances");
+    console.log(
+      `ETH '${parseFloat(ethers.utils.formatEther(balances.ethBalance)).toFixed(
+        precision
+      )}'`
+    );
+    console.log(
+      `MASA '${parseFloat(
+        ethers.utils.formatEther(balances.masaBalance)
+      ).toFixed(precision)}'`
+    );
+    console.log(
+      `USDC '${parseFloat(
+        ethers.utils.formatEther(balances.usdcBalance)
+      ).toFixed(precision)}'`
+    );
+    console.log(
+      `WETH '${parseFloat(
+        ethers.utils.formatEther(balances.wethBalance)
+      ).toFixed(precision)}'`
+    );
   }
 };
