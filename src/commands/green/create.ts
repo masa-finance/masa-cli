e3d1043fc1b1275fb2e9c0157050fc5d3f0082d7
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2023-04-13 10:39:04 +0200

    refactor typings refactor error handling

--- src/commands/green/create.ts
+++ src/commands/green/create.ts
@@ -1,62 +1,69 @@
 import { masa, readLine } from "../../helpers";
-import { PaymentMethod } from "@masa-finance/masa-sdk";
+import {
+  BaseResult,
+  GenerateGreenResult,
+  PaymentMethod,
+  VerifyGreenResult,
+} from "@masa-finance/masa-sdk";
 
 export const create = async (
   paymentMethod: PaymentMethod,
   phoneNumber: string
-) => {
+): Promise<void> => {
   console.log(`Creating Green for phone number: '${phoneNumber}'`);
 
-  const generateResult = await masa.green.generate(phoneNumber);
+  const generateResult: GenerateGreenResult = await masa.green.generate(
+    phoneNumber
+  );
 
-  if (generateResult) {
-    if (generateResult.success) {
-      let verifyGreenResult;
-      do {
-        const code = await readLine(
-          "The code that has been sent to your phone number: "
-        );
-        verifyGreenResult = await masa.green.verify(phoneNumber, code);
+  if (generateResult.success) {
+    let verifyGreenResult: VerifyGreenResult | undefined;
 
-        if (verifyGreenResult) {
-          if (!verifyGreenResult.success) {
-            console.error(
-              `Verifying Green failed! '${verifyGreenResult.message}'`
-            );
-          }
+    do {
+      const code: string = await readLine(
+        "The code that has been sent to your phone number: "
+      );
+      verifyGreenResult = await masa.green.verify(phoneNumber, code);
 
-          if (verifyGreenResult.status === "429") {
-            return;
-          }
-        }
-      } while (!verifyGreenResult?.success);
-
-      if (
-        verifyGreenResult &&
-        verifyGreenResult.authorityAddress &&
-        verifyGreenResult.signatureDate &&
-        verifyGreenResult.signature
-      ) {
-        let mintGreenResult;
-        do {
-          console.log(`Minting Green on '${masa.config.networkName}'`);
-
-          mintGreenResult = await masa.green.mint(
-            paymentMethod,
-            verifyGreenResult.authorityAddress,
-            verifyGreenResult.signatureDate,
-            verifyGreenResult.signature
+      if (verifyGreenResult) {
+        if (!verifyGreenResult.success) {
+          console.error(
+            `Verifying Green failed! '${verifyGreenResult.message}'`
           );
+        }
 
-          if (mintGreenResult && mintGreenResult.tokenId) {
-            console.log(
-              `Green successfully minted on '${masa.config.networkName}' with token ID: '${mintGreenResult.tokenId}'`
-            );
-          }
-        } while (!mintGreenResult?.tokenId);
+        if (verifyGreenResult.status === "429") {
+          return;
+        }
       }
-    } else {
-      console.error(`Generating Green failed: '${generateResult.message}'`);
+    } while (!verifyGreenResult?.success);
+
+    if (
+      verifyGreenResult &&
+      verifyGreenResult.authorityAddress &&
+      verifyGreenResult.signatureDate &&
+      verifyGreenResult.signature
+    ) {
+      let mintGreenResult: BaseResult;
+
+      do {
+        console.log(`Minting Green on '${masa.config.networkName}'`);
+
+        mintGreenResult = await masa.green.mint(
+          paymentMethod,
+          verifyGreenResult.authorityAddress,
+          verifyGreenResult.signatureDate,
+          verifyGreenResult.signature
+        );
+
+        if (mintGreenResult.tokenId) {
+          console.log(
+            `Green successfully minted on '${masa.config.networkName}' with token ID: '${mintGreenResult.tokenId}'`
+          );
+        }
+      } while (!mintGreenResult.tokenId);
     }
+  } else {
+    console.error(`Generating Green failed: '${generateResult.message}'`);
   }
 };
