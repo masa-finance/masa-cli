b7449040056ebeb5b8169e82e402849226e92add
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2023-04-28 11:32:44 +0200

    fix build

--- src/commands/sbt/info.ts
+++ src/commands/sbt/info.ts
@@ -1,16 +1,14 @@
 import { masa } from "../../helpers";
 
 export const info = async (address: string) => {
-  const { selfSovereignSBT } = await masa.contracts.sbt.connect(address);
+  const { sbtContract } = await masa.contracts.sbt.connect(address);
 
-  if (selfSovereignSBT) {
+  if (sbtContract) {
     console.log("Self Sovereign SBT Contract Information:\n");
-    console.log(`Contract Name: '${await selfSovereignSBT.name()}'`);
-    console.log(`Contract Symbol: '${await selfSovereignSBT.symbol()}'`);
-    console.log(`Contract Address: '${selfSovereignSBT.address}'`);
-    console.log(
-      `Total SBTs: ${(await selfSovereignSBT.totalSupply()).toNumber()}`
-    );
+    console.log(`Contract Name: '${await sbtContract.name()}'`);
+    console.log(`Contract Symbol: '${await sbtContract.symbol()}'`);
+    console.log(`Contract Address: '${sbtContract.address}'`);
+    console.log(`Total SBTs: ${(await sbtContract.totalSupply()).toNumber()}`);
     console.log(`Network: '${masa.config.networkName}'`);
   } else {
     console.error(
