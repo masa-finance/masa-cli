6d49e45270d96853bb81dee2543ea58dbf0961b4
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2023-11-28 11:30:55 +0100

    fix: harden logging

--- src/commands/login.ts
+++ src/commands/login.ts
@@ -4,7 +4,7 @@ import { config } from "../utils/config";
 export const login = async () => {
   const result = await masa.session.login();
 
-  if (result) {
+  if (result.cookie) {
     config.set("cookie", result.cookie);
   }
 };
